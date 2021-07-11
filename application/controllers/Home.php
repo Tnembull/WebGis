daf087021742a0c0d26fe60d6ed1af2b5db211d7
Tnembull <mnashiddiqi01@gmail.com>
2021-07-12 03:50:20 +0700

    Tambah dan mengubah data menggunakan modal

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -20,31 +20,11 @@ class Home extends CI_Controller {
 		$this->load->view('pages/data_wisata',$data);
 		$this->load->view('layouts/footer');
 	}
-	public function tambah_data()
-	{
-		# Method Untuk menampilkan form tambah data
-		$data['wisata'] = $this->M_wisata->allData();
-		$this->load->view('layouts/header');
-		$this->load->view('layouts/sidebar');
-		$this->load->view('layouts/topbar');
-		$this->load->view('pages/tambah_data',$data);
-		$this->load->view('layouts/footer');
-	}
 	public function proses_tambah_data()
 	{
 		# method tambah data ke database
 		$this->M_wisata->proses_tambah_data();
 		redirect('Home/data_wisata');
-	}
-	public function edit_data($id)
-	{
-		# method Untuk menampilkan form edit data
-		$data['wisata'] = $this->M_wisata->ambil_id_wisata($id);
-		$this->load->view('layouts/header');
-		$this->load->view('layouts/sidebar');
-		$this->load->view('layouts/topbar');
-		$this->load->view('pages/edit_data',$data);
-		$this->load->view('layouts/footer');
 	}
 
 	public function proses_edit_data()
--- application/views/pages/data_wisata.php
+++ application/views/pages/data_wisata.php
@@ -5,7 +5,10 @@
                 	<h1 class="h3 mb-4 text-gray-800">Data Wisata</h1>
                 	<div class="card shadow mb-4">
                 		<div class="card-header py-3">
-                            <a href="<?=base_url('Home/tambah_data');?>" class="btn btn-primary btn-small">Tambah Data</a>
+                			<button type="button" class="btn btn-primary" data-toggle="modal"
+                				data-target="#exampleModal">
+                				Tambah Data
+                			</button>
                 		</div>
                 		<div class="card-body">
                 			<div class="table-responsive">
@@ -31,8 +34,13 @@
                 							<td><?= $r['alamat']; ?></td>
                 							<td><?= $r['harga']; ?></td>
                 							<td>
-                								<a href="<?= base_url();?>Home/edit_data/<?= $r['id']; ?>" class="btn-circle btn-sm btn-warning"><i class="fas fa-pencil-alt"></i></a>
-                								<a href="<?= base_url();?>Home/hapus_data/<?= $r['id']; ?>" class="btn-circle btn-sm btn-danger"><i class="fas fa-trash-alt"></i></a>
+                								<button type="button" class="btn-circle btn-sm btn-warning" data-toggle="modal"
+                									data-target="#editmodal<?= $r['id']?>">
+                									Edit
+                								</button>
+                								<a href="<?= base_url();?>Home/hapus_data/<?= $r['id']; ?>"
+                									class="btn-circle btn-sm btn-danger"><i
+                										class="fas fa-trash-alt"></i></a>
                 							</td>
                 						</tr>
                 					</tbody>
@@ -47,3 +55,115 @@
 
                 </div>
                 <!-- End of Main Content -->
+
+
+                <!-- Modal Tambah Data -->
+                <!-- Modal -->
+                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog"
+                	aria-labelledby="exampleModalLabel" aria-hidden="true">
+                	<div class="modal-dialog" role="document">
+                		<div class="modal-content">
+                			<div class="modal-header">
+                				<h5 class="modal-title" id="exampleModalLabel">Form Tambah Data</h5>
+                				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                					<span aria-hidden="true">&times;</span>
+                				</button>
+                			</div>
+                			<div class="modal-body">
+                				<?= form_open_multipart('Home/proses_tambah_data');?>
+                				<div class="form-group row">
+                					<label for="nama" class="col-sm-2 col-form-label">Nama</label>
+                					<div class="col">
+                						<input type="text" class="form-control" id="nama" name="nama"
+                							placeholder="ex: Pantai Mutun ">
+                					</div>
+                				</div>
+                				<div class="form-group row">
+                					<label for="cord" class="col-sm-2 col-form-label">Cordinate</label>
+                					<div class="col">
+                						<input type="text" class="form-control" id="cord" name="cord"
+                							placeholder="ex: 8.3405,115.0920">
+                					</div>
+                				</div>
+                				<div class="form-group row">
+                					<label for="alamat" class="col-sm-2 col-form-label">Alamat</label>
+                					<div class="col">
+                						<input type="text" class="form-control" id="alamat" name="alamat"
+                							placeholder="ex: Jalan Hanura">
+                					</div>
+                				</div>
+                				<div class="form-group row">
+                					<label for="harga" class="col-sm-2 col-form-label">Harga</label>
+                					<div class="col">
+                						<input type="number" class="form-control" id="harga" name="harga"
+                							placeholder="ex: 35000">
+                					</div>
+                				</div>
+                			</div>
+                			<div class="modal-footer">
+                				<button type="reset" class="btn btn-danger">Reset</button>
+                				<button type="submit" class="btn btn-primary">Simpan</button>
+                				<?= form_close();?>
+                			</div>
+                		</div>
+                	</div>
+                </div>
+                <!-- Akhir Modal Tambah Data -->
+
+
+				<!-- Modal Edit Data -->
+                <!-- Modal -->
+				<?php 
+				$no = 0;
+				foreach ($wisata as $r): $no++; ?>
+                <div class="modal fade" id="editmodal<?= $r['id']; ?>" tabindex="-1" role="dialog"
+                	aria-labelledby="exampleModalLabel" aria-hidden="true">
+                	<div class="modal-dialog" role="document">
+                		<div class="modal-content">
+                			<div class="modal-header">
+                				<h5 class="modal-title" id="exampleModalLabel">Form Edit Data</h5>
+                				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                					<span aria-hidden="true">&times;</span>
+                				</button>
+                			</div>
+                			<div class="modal-body">
+                				<?= form_open_multipart('Home/proses_edit_data');?>
+								<input type="hidden" name="id" value="<?= $r['id'];?>">
+                				<div class="form-group row">
+                					<label for="nama" class="col-sm-2 col-form-label">Nama</label>
+                					<div class="col">
+                						<input type="text" class="form-control" id="nama" name="nama"
+										value="<?= $r['nama'];?>">
+                					</div>
+                				</div>
+                				<div class="form-group row">
+                					<label for="cord" class="col-sm-2 col-form-label">Cordinate</label>
+                					<div class="col">
+                						<input type="text" class="form-control" id="cord" name="cord"
+										value="<?= $r['cord'];?>">
+                					</div>
+                				</div>
+                				<div class="form-group row">
+                					<label for="alamat" class="col-sm-2 col-form-label">Alamat</label>
+                					<div class="col">
+                						<textarea type="text" class="form-control" rows="3" id="alamat" name="alamat"
+										value=""><?= $r['alamat'];?></textarea>
+                					</div>
+                				</div>
+                				<div class="form-group row">
+                					<label for="harga" class="col-sm-2 col-form-label">Harga</label>
+                					<div class="col">
+                						<input type="number" class="form-control" id="harga" name="harga"
+										value="<?= $r['harga'];?>">
+                					</div>
+                				</div>
+                			</div>
+                			<div class="modal-footer">
+                				<button type="submit" class="btn btn-primary">Ubah</button>
+                				<?= form_close();?>
+                			</div>
+                		</div>
+                	</div>
+                </div>
+				<?php endforeach; ?>
+                <!-- Akhir Modal Edit Data -->
